9019a380e64fe590cfb913a2edfb8cbd2e8a26b7
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-04-04 03:07:15 +0900

    chore: comment

--- app/app.go
+++ app/app.go
@@ -205,14 +205,14 @@ func New(
 	// build app
 	app.App = appBuilder.Build(db, traceStore, baseAppOptions...)
 
-	// WasmのStoreKeyを登録
+	// Register wasm's StoreKey
 	if err := app.RegisterStores(
 		storetypes.NewKVStoreKey(wasmtypes.StoreKey),
 	); err != nil {
 		panic(err)
 	}
 
-	// IBCを手動で登録
+	// Register IBC modules manually
 	if err := app.registerIBCModules(appOpts); err != nil {
 		panic(err)
 	}
