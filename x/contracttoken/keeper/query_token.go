a1fc5c17ec3f93cc44b67d2232c6d4d68684c90e
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-04-10 01:29:11 +0900

    feat: token query

--- x/contracttoken/keeper/query_token.go
+++ x/contracttoken/keeper/query_token.go
@@ -3,13 +3,53 @@ package keeper
 import (
 	"context"
 
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/exoralayer/exora/x/contracttoken/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (q queryServer) Token(ctx context.Context, req *types.QueryTokenRequest) (*types.QueryTokenResponse, error) {
-	return &types.QueryTokenResponse{}, nil
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	contractAddr, err := sdk.AccAddressFromBech32(req.ContractAddress)
+	if err != nil {
+		return nil, errorsmod.Wrap(err, "invalid contract address")
+	}
+
+	token, err := q.k.GetToken(ctx, contractAddr)
+	if err != nil {
+		return nil, err
+	}
+	return &types.QueryTokenResponse{Token: &token}, nil
 }
 
 func (q queryServer) Tokens(ctx context.Context, req *types.QueryTokensRequest) (*types.QueryTokensResponse, error) {
-	return &types.QueryTokensResponse{}, nil
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	tokens, pageRes, err := query.CollectionPaginate(
+		ctx,
+		q.k.Tokens,
+		req.Pagination,
+		func(key sdk.AccAddress, value types.Token) (types.Token, error) {
+			return value, nil
+		},
+	)
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	tokenPointers := make([]*types.Token, len(tokens))
+	for i, token := range tokens {
+		tokenPointers[i] = &token
+	}
+
+	return &types.QueryTokensResponse{Tokens: tokenPointers, Pagination: pageRes}, nil
 }
